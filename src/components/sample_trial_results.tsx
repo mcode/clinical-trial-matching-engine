03b81d90c98e4df73793cd58f6d276a74f9a053d
Levine, Lauren <lelevine@mitre.org>
2020-03-26 12:34:00 -0400

    updates for unpacking base and avdanced results

--- src/components/sample_trial_results.tsx
+++ src/components/sample_trial_results.tsx
@@ -16,7 +16,30 @@ export const unpackBaseMatchResults = () => {
   sample.data.baseMatches.edges.forEach( match => {
 
     let trial = match.node
-    recursiveUnpacking(data, trial);
+    let mainRow:object = {};
+    let sites:object = {};
+    let blackList = ["advancedMatchConditions", "armGroups", "interventions"];
+
+    Object.keys(trial).forEach( field => {
+      if ( field === "conditions") {
+        let value = trial[field];
+        value = clearPunctuation(value);
+        mainRow[field] = value;
+      } else if (field === "meshTerms") {
+        let termsList = trial[field].join(", ");
+        mainRow[field] = termsList;
+      } else if (field === "sites") {
+        sites = trial[field];
+      } else if (!blackList.includes(field)) {
+        mainRow[field] = trial[field];
+      }
+    });
+
+    data.push(mainRow);
+
+    Object.keys(sites).forEach( index => {
+      data.push(sites[index]);
+    });
 
   });
 
@@ -32,92 +55,27 @@ export const unpackAdvancedMatchResults = () => {
     let quality = match.matchQuality
 
     let mainRow:object = {"Match Quality" : quality};
-    let subsequent:object[] = [];
+    let sites:object = {};
 
     Object.keys(trial).forEach( field => {
-      if ( typeof trial[field] !== "object" ) {
+      if ( field === "conditions") {
         let value = trial[field]
-        if (field === "conditions"){
-          value = clearPunctuation(value)
-        }
+         value = clearPunctuation(value)
         mainRow[field] = value;
+      } else if (field === "sites") {
+        sites = trial[field];
       } else {
-        subsequent.push(trial[field]);
+        mainRow[field] = trial[field];
       }
     });
 
     data.push(mainRow);
-    subsequent.forEach( complex_field => {
-      Object.keys(complex_field).forEach( index => {
-        data.push(complex_field[index])
-      });
+
+    Object.keys(sites).forEach( index => {
+      data.push(sites[index]);
     });
 
   });
 
   return data;
 }
-
-const recursiveUnpacking = (data, unravel) => {
-
-  let mainRow:object = {};
-  let subsequent:object[] = [];
-
-  console.log(unravel)
-
-  Object.keys(unravel).forEach( field => {
-    if ( typeof unravel[field] === "object" && unravel[field] !== null) {
-      subsequent.push(unravel[field]);
-    } else {
-      let value = unravel[field]
-      if (field === "conditions"){ // value.includes("\"")
-        value = clearPunctuation(value)
-      }
-      mainRow[field] = value;
-    }
-  });
-
-  data.push(mainRow);
-  if (subsequent.length === 0){
-    return;
-  } else {
-    subsequent.forEach( complex_field => {
-      Object.keys(complex_field).forEach( index => {
-        let num_test = Number(index)
-        if (!isNaN(num_test) && typeof complex_field[index] === "object") { // object is an array
-          recursiveUnpacking(data, complex_field[index])
-        } else { // object is an object
-          recursiveUnpacking(data, complex_field)
-        }
-
-      });
-    });
-  }
-
-}
-
-/*
-export const sample_trial_results = () => {
-
-  let data:object[] = [];
-  data.push({"Match Count" : sample["data"]["baseMatches"]["totalCount"]});
-  sample["data"]["baseMatches"]["edges"].forEach( node => {
-
-    let trial = node["node"]
-    let conditions = clearPunctuation(trial["conditions"])
-
-    data.push({"nctId" : trial["nctId"], "title" : trial["title"], "conditions" : conditions,
-               "gender" : trial["gender"], "phase" : trial["phase"], "minimumAge" : trial["minimumAge"],
-               "maximumAge" : trial["maximumAge"]});
-
-    trial["sites"].forEach( site => {
-
-      data.push({"facility" : site["facility"], "contactName" : site["contactName"],
-      "contactEmail" : site["contactEmail"], "contactPhone" : site["contactPhone"], "latitude" : site["latitude"],
-      "longitude" : site["longitude"]})
-
-    });
-  });
-  return data;
-}
-*/
