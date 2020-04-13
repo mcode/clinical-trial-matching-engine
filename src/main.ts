7effc52e776afd2a9e001862cdd4ccec2ad29034
Dan Potter <dpotter@mitre.org>
2020-04-13 10:50:53 -0400

    Make VS Code happier

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import FHIR from 'fhirclient';
+import * as FHIR from 'fhirclient';
 
 if (environment.production) {
   enableProdMode();
