23aafe5668ba336dc485b1ac3e26083ad63ec084
Dan Potter <dpotter@mitre.org>
2020-04-13 10:45:23 -0400

    Start the FHIR client on launch

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import FHIR from 'fhirclient';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// Let the FHIR client do its thing before starting Angular
+FHIR.oauth2
+  .init({
+    clientId: 'Input client id you get when you register the app',
+    scope: 'launch/patient openid profile'
+  })
+  .then(client => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  })
+  .catch(error => {
+    // TODO: Handle this in a user-visible way
+    console.error(error)
+  });
