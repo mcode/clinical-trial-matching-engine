7b276f7e2506a6643086c44f426df4e3af35a016
Dan Potter <dpotter@mitre.org>
2020-06-05 14:34:02 -0400

    Simple fixes to the details page

--- src/app/result-details/result-details.component.ts
+++ src/app/result-details/result-details.component.ts
@@ -16,12 +16,26 @@ export class ResultDetailsComponent implements OnInit {
   ngOnInit() {
   }
 
+  public get trialSaved(): boolean {
+    // FIXME: Actually determine if the trial is saved
+    return false;
+  }
+
+  public toggleTrialSaved(): void {
+    // FIXME: Actually toggle the save status of the trial (probably going to
+    // be an @Output event)
+  }
+
+  public replace(value: string): string {
+    return value.replace(/[\[\]_'""]+/g, ' ');
+  }
+
   /**
    * Toggle visibility of accordion sections (specifically, looks for next
-   * sibling that is a parent and then toggles the visibility of the display
+   * sibling that is a panel and then toggles the visibility of the display
    * on its style element).
    */
-  public showHideAccordian(event) {
+  public showHideAccordian(event): void {
     for (let sibling = event.target.nextElementSibling; sibling; sibling = sibling.nextElementSibling) {
       if (/\bpanel\b/.test(sibling.className)) {
         if (sibling.style.display === 'block') {
