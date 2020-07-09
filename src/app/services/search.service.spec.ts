ba1065219372a9191306722edc2ff4018fe2b19b
Dan Potter <dpotter@mitre.org>
2020-07-09 13:48:18 -0400

    Use test HTTP client service

--- src/app/services/search.service.spec.ts
+++ src/app/services/search.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [SearchService]
+  }));
 
   it('should be created', () => {
     const service: SearchService = TestBed.get(SearchService);
