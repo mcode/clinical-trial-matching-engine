00f05212d164a20e36fa94cd4373f904034b53b1
lelevine <lelevine@mitre.org>
2020-10-01 12:16:07 -0400

    adding prettier and fixing lint errors

--- src/app/smartonfhir/client.service.ts
+++ src/app/smartonfhir/client.service.ts
@@ -21,22 +21,28 @@ export class ClientService {
   client: Client;
   patient: Patient;
   private pendingClient: Promise<Client> | null = null;
-  public resourceTypes = [
-    'Patient',
-    'Condition',
-    'MedicationStatement',
-    'Observation',
-    'Procedure'
-  ];
+  public resourceTypes = ['Patient', 'Condition', 'MedicationStatement', 'Observation', 'Procedure'];
   // Setting filters to pull only resources of relevant mcode profiles
   // {'_profile:below': 'http://hl7.org/fhir/us/mcode'} this should work for all the resource types but it doesn't and I don't know why, so we're listing profiles - maybe the moonshot fhir server doesn't implement 'below' as a search modifier
   // Note: did not include all observation mcode profiles
   public resourceParams = {
     Patient: {},
-    Condition: { 'clinical-status': 'active', '_profile': 'http://hl7.org/fhir/us/mcode/StructureDefinition/obf-ComorbidCondition,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-primary-cancer-condition,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-secondary-cancer-condition' },
-    MedicationStatement: {'_profile': 'http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-medication-statement,'},
-    Observation: {'_profile': ' http://hl7.org/fhir/us/mcode/StructureDefinition/onco-core-CancerDiseaseStatus,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-stage-group,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-stage-group,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tumor-marker'},
-    Procedure: {'_profile': 'http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-radiation-procedure,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-surgical-procedure'}
+    Condition: {
+      'clinical-status': 'active',
+      _profile:
+        'http://hl7.org/fhir/us/mcode/StructureDefinition/obf-ComorbidCondition,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-primary-cancer-condition,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-secondary-cancer-condition'
+    },
+    MedicationStatement: {
+      _profile: 'http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-medication-statement,'
+    },
+    Observation: {
+      _profile:
+        ' http://hl7.org/fhir/us/mcode/StructureDefinition/onco-core-CancerDiseaseStatus,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-stage-group,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-stage-group,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tumor-marker'
+    },
+    Procedure: {
+      _profile:
+        'http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-radiation-procedure,http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-surgical-procedure'
+    }
   };
   /**
    * Gets a Promise that resolves to the client when the client is ready. If
