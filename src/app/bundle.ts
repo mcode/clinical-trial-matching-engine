ad41709f2dcabbca92ab0051066fa4e00ad3a330
RobertScalfani <robis345@gmail.com>
2020-11-09 14:25:52 -0500

    Changed the patient bundle export to a JSON rather than a string.

--- src/app/bundle.ts
+++ src/app/bundle.ts
@@ -12,41 +12,30 @@ export function createPatientBundle(
   parameters: { [key: string]: Stringable },
   entries: fhirclient.FHIR.BundleEntry[]
 ): string {
-  let paramResource = `{
-                         "resourceType": "Parameters",
-                         "id": "0",
-                         "parameter": [`;
+
+  let paramResource: ParameterResource = {resourceType: "Parameters", id: "0", parameter: []};
   for (const p in parameters) {
     // Ignore null values, they indicate a parameter that should be ignored.
     const value = parameters[p];
     if (value === null) continue;
-    paramResource += `{
-                        "name": ${JSON.stringify(p)},
-                        "valueString": ${JSON.stringify(value)}
-                      },`;
+    paramResource.parameter.push({name: p, valueString: value});
   }
-  // need to remove final comma
-  paramResource = paramResource.slice(0, -1);
-  paramResource += `
-                     ]
-                    }
-                  },`;
-  let patientBundle =
-    `{
-                          "resourceType": "Bundle",
-                          "type": "collection",
-                          "entry": [
-                            {
-                              "resource": ` + paramResource;
-  //for (const resource in entries)
+  let patientBundle: PatientBundle = {resourceType: 'Bundle', type: 'collection', entry: [{resource: paramResource}]};
   entries.forEach((resource) => {
-    // for each instead
-    patientBundle += `
-    ${JSON.stringify({ fullUrl: resource.fullUrl, resource: resource.resource })},`;
+    patientBundle.entry.push({fullUrl: resource.fullUrl, resource: resource.resource});
   });
-  patientBundle = patientBundle.slice(0, -1);
-  patientBundle += `
-                      ]
-                     }`;
-  return patientBundle;
+  console.log(JSON.stringify(patientBundle));
+  return JSON.stringify(patientBundle);
+}
+
+export interface ParameterResource {
+  resourceType?: string;
+  id?: string;
+  parameter?: {name: string, valueString: Stringable}[];
+}
+
+export interface PatientBundle {
+  resourceType?: string;
+  type?: string;
+  entry?: {resource: ParameterResource }[] | {fullUrl:string, resource: fhirclient.FHIR.Resource}[];
 }
