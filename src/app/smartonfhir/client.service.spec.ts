814deb6962ac08423154288751c247844696b789
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-11-29 21:48:13 -0500

    client service tests

--- src/app/smartonfhir/client.service.spec.ts
+++ src/app/smartonfhir/client.service.spec.ts
@@ -9,4 +9,36 @@ describe('ClientService', () => {
     const service: ClientService = TestBed.get(ClientService);
     expect(service).toBeTruthy();
   });
+  it('should get client', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getClient()).toBeDefined();
+
+  });
+  it('should get patient', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getPatient()).toBeDefined();
+
+  });
+  it('should get record', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getRecords("")).toBeDefined();
+
+  });
+  it('should get all records', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getRecords("")).toBeDefined();
+
+  });
+  it('should get conditions', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getConditions()).toBeDefined();
+
+  });
+  it('should get resources', () =>{
+    const service: ClientService = TestBed.get(ClientService);
+    expect(service.getResources("")).toBeDefined();
+
+  });
+
+
 });
