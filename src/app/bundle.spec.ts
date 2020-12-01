b3416b8922b5ce606aa1ae57b013752b9edac6b3
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-11-30 22:53:23 -0500

    coverage for bundle ts

---
+++ src/app/bundle.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PatientBundle, createPatientBundle } from './bundle';
+
+describe('bundle tests', () => {
+  beforeEach(() => TestBed.configureTestingModule({ imports: [HttpClientTestingModule], providers: [] }));
+
+  it('should create the patient bundle', () => {
+    expect(createPatientBundle({ id: 'example' }, [])).toBeDefined();
+  });
+});
