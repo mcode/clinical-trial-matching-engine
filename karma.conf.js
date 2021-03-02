cc62abf6145cec1331c309df9aed80e493727f2c
Dan Potter <dpotter@mitre.org>
2021-03-02 14:08:42 -0500

    Upgrade to karma-coverage
    Replaces deprecated karma-coverage-istanbul-reporter with the
    replacement karma-coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/5.2/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -9,16 +9,19 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/clinicalTrial'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
