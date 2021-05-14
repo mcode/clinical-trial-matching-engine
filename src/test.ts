18ac186a3737a8215b40ba0787d3fadb89056eb3
Dan Potter <dpotter@mitre.org>
2021-05-14 17:04:04 -0400

    Update conf/dependencies

--- src/test.ts
+++ src/test.ts
@@ -4,6 +4,17 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 // Then we find all the tests.
