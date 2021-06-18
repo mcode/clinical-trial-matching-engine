13a0ca2bf2311dd012d72c19ef9fb686094237ec
Dan Potter <dpotter@mitre.org>
2021-06-18 10:40:58 -0400

    Fix app-config to work in "real" environment

--- src/app/services/app-config.service.spec.ts
+++ src/app/services/app-config.service.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AppConfigService } from './app-config.service';
+import { AppConfigService, SearchProviders } from './app-config.service';
 
 describe('AppConfigService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
+  it('uses the injected SearchProviders if they exist', () => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: SearchProviders,
+          useValue: new SearchProviders([{ name: 'TestService', url: 'https://www.example.com/test-service' }])
+        }
+      ]
+    });
     const service: AppConfigService = TestBed.inject(AppConfigService);
-    expect(service).toBeDefined();
+    const providers = service.getSearchProviders();
+    expect(providers.length).toEqual(1);
+    expect(providers[0].name).toEqual('TestService');
+    expect(providers[0].url).toEqual('https://www.example.com/test-service');
   });
-  it('should get configured services', () => {
+
+  it('gets configured services from the environment if none are provided', () => {
+    TestBed.configureTestingModule({});
     const service: AppConfigService = TestBed.inject(AppConfigService);
     expect(Array.isArray(service.getSearchProviders())).toBeTrue();
   });
--- src/app/services/app-config.service.ts
+++ src/app/services/app-config.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Optional } from '@angular/core';
+import { Inject, Injectable, Optional } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 
@@ -18,6 +18,22 @@ export class SearchProvider {
 }
 
 /**
+ * This class exists solely to allow servers to be injected to the app config.
+ * To use it, you need to provide your own provider, as it is not injected
+ * anywhere by default.
+ *
+ * For example:
+ * {
+ *   provide: SearchProviders,
+ *   useValue: new SearchProviders([{ name: 'TestService', url: 'https://www.example.com' }])
+ * }
+ */
+@Injectable()
+export class SearchProviders {
+  constructor(public servers: SearchProviderJSON[]) {}
+}
+
+/**
  * Provides a method for accessing global app configuration. Currently pulls
  * from the environment, but is accessed as a service to enable it to be
  * transitioned over to some other method in the future.
@@ -27,9 +43,15 @@ export class SearchProvider {
 })
 export class AppConfigService {
   private _servers: SearchProviderJSON[];
-  constructor(@Optional() servers?: SearchProviderJSON[]) {
+  // Because we specify the constructor as allowing the interface (basically, so you can use it with a simple JSON
+  // object), we need to tell Angular what to use for dependency injection.
+  constructor(@Inject(SearchProviders) @Optional() servers?: SearchProviders | SearchProviderJSON[] | null) {
     // If given servers, use that
-    this._servers = servers ? servers : environment.servers;
+    if (servers instanceof SearchProviders) {
+      this._servers = servers.servers;
+    } else {
+      this._servers = servers ? servers : environment.servers;
+    }
   }
   /**
    * Gets configured search providers.
